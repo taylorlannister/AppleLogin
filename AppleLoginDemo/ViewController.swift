f5ad5197a1a5f3032f3307fd591ee157f195f6cd
刘志康 <15915872210@163.com>
2020-05-11 14:21:51 +0800

    创建项目文件

---
+++ AppleLoginDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleLoginDemo
+//
+//  Created by 刘志康 on 2020/5/11.
+//  Copyright © 2020 刘志康. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppleLoginDemoTests/AppleLoginDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppleLoginDemoTests.swift
+//  AppleLoginDemoTests
+//
+//  Created by 刘志康 on 2020/5/11.
+//  Copyright © 2020 刘志康. All rights reserved.
+//
+
+import XCTest
+@testable import AppleLoginDemo
+
+class AppleLoginDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
